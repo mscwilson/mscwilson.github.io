2650ea230a2857a88f794ec4a66a0448fa343189
Miranda Wilson <mirandascwilson@gmail.com>
2021-06-02 21:00:42 +0100

    Replace sp.js with older version

--- assets/js/testPage.js
+++ assets/js/testPage.js
@@ -15,12 +15,29 @@ function linkNowhere() {
 function linkSimilar() {
   console.log("in linkSimilar");
 
+  // snowplow('trackSelfDescribingEvent', {
+  //   schema: 'iglu:test.mwilson/test-link/jsonschema/1-0-0',
+  //   data: {
+  //     linkId: "actually just a string for now"
+  //   }
+  // });
+
   snowplow('trackSelfDescribingEvent', {
-    schema: 'iglu:test.mwilson/test-link/jsonschema/1-0-0',
-    data: {
-      linkId: "actually just a string for now"
-    }
-  });
+            schema: 'iglu:test.example.iglu/cart_action_event/jsonschema/1-0-0',
+            data: {
+                type: "add"
+            }
+        },
+        [{
+            schema: 'iglu:test.example.iglu/product_entity/jsonschema/1-0-0',
+            data: {
+                sku: 'sku',
+                name: 'test',
+                price: 0.01,
+                quantity: 1
+            }
+        }]
+    );
 }
 
 const link = document.getElementById("test-link");
@@ -28,3 +45,5 @@ link.addEventListener("mouseover", linkNowhere);
 
 const anotherLink = document.getElementById("test-link2");
 anotherLink.addEventListener("click", linkSimilar);
+
+
